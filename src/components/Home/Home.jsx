a560d608dd914af30e968d382f60ef45ebc30271
Krys Frost <librarykrystal@gmail.com>
2024-01-22 00:38:37 -0600

    Added 2-row cover image and conditional render on /home based on screen width.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useLayoutEffect } from "react";
 
 import Bulletin from '../Bulletin/Bulletin';
-import homeImage from './artSiteCoverImages.png';
+import homeImage1 from './artSiteCoverImages.png';
+import homeImage2 from './artSiteImagesCover2rows.png';
 
 // Material UI Imports
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -37,6 +38,14 @@ const theme = createTheme({
   },
 });
 
+// The current width of the viewport
+const width = window.innerWidth;
+console.log('width:', width);
+
+// The width below which the multi-row home image should be rendered
+const breakpoint = 900;
+
+
 function Home() {
 
   // Makes each view load scrolled to top
@@ -47,7 +56,14 @@ function Home() {
   return (
     <ThemeProvider theme={theme}>
       <div className="homeContainer">
-        <img className="coverImage" src={homeImage}></img>
+        {/* <img className="coverImage" src={homeImage}></img> */}
+
+        {/* CONDITIONAL RENDER — shows appropriate image for screen size */}
+        { width >= breakpoint ? 
+          <img className="coverImage" src={homeImage1}></img>
+        :
+          <img className="coverImage" src={homeImage2}></img>
+        }
 
       </div>
     </ThemeProvider>
