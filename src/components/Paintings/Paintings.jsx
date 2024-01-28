df084cd95a1f4e9f6995e373ffa17a9b0f49f0cf
Krys Frost <librarykrystal@gmail.com>
2024-01-27 20:34:59 -0600

    Moved content from Projects.jsx into Paintings.jsx, added array of objects for works, and updated CSS accordingly.

--- src/components/Paintings/Paintings.jsx
+++ src/components/Paintings/Paintings.jsx
@@ -3,11 +3,24 @@ import React, { useLayoutEffect } from "react";
 import Bulletin from '../Bulletin/Bulletin';
 import Projects from '../Projects/Projects';
 
+// imports of latest work and zoom-in images
 import hush from './webHush.png';
 import hushZoomCat from './webHushZoomCatScan.png';
 import hushZoomSkull from './webHushZoomSkullHandScan.png';
 import hushZoomTendrils from './webHushZoomTendrilsScan.png';
 
+// imports of older portraiture works
+import fitzgerald from './webFitzgeraldE.png';
+import hitchcock from './webHitchcockA.png';
+import nabokov from './webNabokovV.png';
+import poe from './webPoeEA.png';
+import prince from './webPrince.png';
+import price from './webPriceV.png';
+import serling from './webSerlingR.png';
+import simone from './webSimoneN.png';
+import welles from './webWellesO.png';
+import wilde from './webWildeO.png';
+import winehouse from './webWinehouseA.png';
 
 // Material UI Imports
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -43,12 +56,29 @@ const theme = createTheme({
   },
 });
 
+const artworks = [
+  {id: 23, title: "Hush", year: 2023, set: "new", origAvailable: false, printAvailable: true, image: hush},
+  {id: 22, title: "Prince", year: 2023, set: "portrait", origAvailable: false, printAvailable: true, image: prince},
+  {id: 21, title: "Amy Winehouse", year: 2022, set: "portrait", origAvailable: false, printAvailable: true, image: winehouse},
+  {id: 20, title: "Nina Simone", year: 2021, set: "portrait", origAvailable: false, printAvailable: true, image: simone},
+  {id: 19, title: "Vladimir Nabokov", year: 2021, set: "portrait", origAvailable: false, printAvailable: true, image: nabokov},
+  {id: 18, title: "Oscar Wilde", year: 2020, set: "portrait", origAvailable: false, printAvailable: true, image: wilde},
+  {id: 17, title: "Edgar Allan Poe", year: 2020, set: "portrait", origAvailable: false, printAvailable: true, image: poe},
+  {id: 15, title: "Vincent Price", year: 2019, set: "portrait", origAvailable: false, printAvailable: true, image: price},
+  {id: 14, title: "Orson Welles", year: 2017, set: "portrait", origAvailable: false, printAvailable: true, image: welles},
+  {id: 13, title: "Ella Fitzgerald", year: 2015, set: "portrait", origAvailable: false, printAvailable: true, image: fitzgerald},
+  {id: 12, title: "Rod Serling", year: 2014, set: "portrait", origAvailable: false, printAvailable: true, image: serling},
+  {id: 10, title: "Alfred Hitchcock", year: 2014, set: "portrait", origAvailable: false, printAvailable: true, image: hitchcock},
+];
+
 function Paintings() {
 
   // Makes each view load scrolled to top
   useLayoutEffect(() => {
     window.scrollTo(0, 0)
   }, []);
+
+  console.log(artworks);
 
   return (
     <ThemeProvider theme={theme}>
@@ -63,8 +93,9 @@ function Paintings() {
           </Typography>
         </div>
 
+        {/* LATEST main image and caption */}
         <img className="latestImageLandscape" src={hush}></img>
-        <div className="caption">
+        <div className="captionUnder">
           <center>
             <Typography component={'span'} variant="body1" sx={{ textAlign: "justify"}}gutterBottom>
               <Box fontWeight="bold" fontStyle="italic" display='inline'>Hush</Box>, 16x20 acrylic on canvas
@@ -76,21 +107,34 @@ function Paintings() {
           </center>
         </div>
 
-        <div className="latestContainer">
-        
-        
-        {/* <div class="lineBreak"></div> */}
-
-        <img className="zoomImage" src={hushZoomTendrils}></img>
-        <img className="zoomImage" src={hushZoomSkull}></img>
-        <img className="zoomImage" src={hushZoomCat}></img>
-
+        {/* LATEST closeup images */}
+        <div className="latestCloseupsContainer">
+          <img className="zoomImage" src={hushZoomTendrils}></img>
+          <img className="zoomImage" src={hushZoomSkull}></img>
+          <img className="zoomImage" src={hushZoomCat}></img>
         </div>
+
+      </div>
+      {/* end main container */}
+
+
+      {/* OLDER / COMPLETED WORKS GALLERY */}
+      <div className="paintingsOuterContainer">
+
+        {/* images of older completd works */}
+        <div className="paintingsSectionContainer">
+         <img className="canvasImagePortrait" src={prince}></img>
+        </div>
+        
+        {/* copyright */}
+        <center>
+          <Typography component={'span'} variant="body1" color="white" gutterBottom>© Krystal Frost</Typography>
+        </center>
+        <br/>
+
       </div>
 
-      {/* Consuming Projects Component */}
-      <Projects />
-
+      
     </ThemeProvider>
   );
 }
